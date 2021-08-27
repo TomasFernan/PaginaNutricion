acbc9b4e832379ec34ae1f2e391775cf39f6f693
Tomas <tomasfernandezwd@gmail.com>
2021-08-27 11:36:30 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PaginaNutricion';
+  title:string = 'Pagina Nutricion';
+
+  clients = [
+    {'name': 'Tomas', 'lastName': 'Fernandez', 'age': '22'},
+    {'name': 'Luciana', 'lastName': 'Godio', 'age': '24'}
+  ]
+
+  model:any = {};
+
+  addClient():void{
+
+  }
+
+  deleteClient():void{
+
+  }
+
+  editClient():void{
+
+  }
+
+  updateClient():void{
+
+  }
+
+
 }
